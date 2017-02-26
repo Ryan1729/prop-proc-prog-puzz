670ba4e30acac0c26cf33a9f8e6593312912fef0
Ryan1729 <Ryan1729@gmail.com>
2017-02-26 00:18:34 -0700

    print a random function to the console
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate rand;
+
+use rand::{Rng, SeedableRng, StdRng};
+
+static mut RNG: Option<StdRng> = None;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng;
+    unsafe {
+        if RNG.is_none() {
+            let seed: &[_] = &[42];
+            RNG = Some(SeedableRng::from_seed(seed));
+        }
+
+        rng = RNG.as_mut().unwrap();
+    }
+
+    let func_str = match rng.gen::<u8>() % 4 {
+        0 => template("false"),
+        1 => template("!b"),
+        2 => template("b"),
+        _ => template("true"),
+    };
+
+    println!("{}", func_str);
+}
+
+fn template(code: &str) -> String {
+    format!(r"
+    pub fn fill_me_in (b : bool) -> bool {{
+        {}
+    }}
+    ",
+            code)
 }
